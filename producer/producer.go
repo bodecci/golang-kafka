580813c5e2a0a9dec4b91da5ce492e7ecf32822e
Bode F <bode.falade@outlook.com>
2024-10-02 22:45:47 -0500

    added struct and main func.

---
+++ producer/producer.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type Comment struct {
+	Text string `form:"text" json:"text"`
+}
+
+func main() {
+	app := fiber.New()
+	api := app.Group("/api/vi")
+
+	api.POST("/comments", createComment)
+
+	app.Listen(":3000")
+}
+
+func createComment(c *fiber.Ctx) error {
+
+}
