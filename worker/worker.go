9d9b1e92aff562621dd10a09845d9dcd8a1b8c48
Bode F <bode.falade@outlook.com>
2024-10-10 14:52:55 -0500

    setup main and connectConsumer call.

--- worker/worker.go
+++ worker/worker.go
@@ -1,17 +1,59 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Shopify/sarama"
+)
+
 func main() {
 	topic := "comments"
 	connectConsumer([]string{"localhost:29092"})
 	if err != nil {
 		panic(err)
 	}
-
+	// Calling ConsumePartition. It will open one connection per broker
+	// and share it for all partitions that live on it.
 	consumer, err := worker.ConsumePartition(topic, 0, sarama.OffsetOldest)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("consumer started")
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+
+	msgCount := 0
+
+	doneCh := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case err := <-consumer.Errors():
+				fmt.Println(err)
+			case msg := <-consumer.Messages():
+				msgCount++
+				fmt.Printf("Received message Count: %d: | Topic (%s) | Message (%s) \n", msgCount, string(msg.Topic), string(msg.Value))
+			case <-sigchan:
+				fmt.Println("Interrupt is detected")
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+
+	<-doneCh
+	fmt.Println("Processed", msgCount, "messages")
+
+	if err := worker.Close(); err != nil {
+		panic(err)
+	}
+
+}
+
+func connectConsumer(brokersUrl []string) (sarama.Consumer, error) {
 
 }
